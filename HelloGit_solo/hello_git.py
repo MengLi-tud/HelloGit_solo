122cee5fe6d85b6122d2bf29ce633bf06baf0e32
Meng <m.li-4@tudelft.nl>
2020-08-13 14:35:56 +0200

    printing numbers upto age

--- HelloGit_solo/hello_git.py
+++ HelloGit_solo/hello_git.py
@@ -3,3 +3,11 @@ print(message)
 
 a = input("what is your name?")
 print("Hi", a, ", Have a great day!")
+
+b = int(input("what is your age?"))
+print(f"You are {b} years young!")
+
+
+#print out all numbers uptil the user's age
+for x in range(b):
+    print(x+1, end=" ")
